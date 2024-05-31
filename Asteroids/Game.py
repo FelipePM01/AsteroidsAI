525a69682505f33048ffa545410384b2d1074aff
gimicaroni <gimicaroni@gmail.com>
2024-05-30 21:04:43 -0300

    funcao play_step adaptada

--- Asteroids/Game.py
+++ Asteroids/Game.py
@@ -2,7 +2,7 @@ import pygame
 import math
 import random
 
-from Asteroids.Asteroids import Player, Saucer
+from Asteroids.Asteroids import Player, Saucer, Asteroid
 
 class Game:
     def __init__(self,fps,model_playing,disable_display) -> None:
@@ -43,8 +43,8 @@ class Game:
         self.snd_extra = pygame.mixer.Sound("Sounds/extra.wav")
         
     def start_game_loop(self):
-        self.gameState = 0
-        self.playerState = 0
+        self.gameState = 0 # 0 = jogando 1 = fim de jogo
+        self.playerState = 0 # 0 = vivo 1 = morto
         self.player_pieces = 0
         self.player_dying_delay = 0
         self.player_invi_dur = 0
@@ -105,7 +105,286 @@ class Game:
             return True
         return False
 
+    def play_step(self, action):
+        # Game menu
+        if self.gameState == 1:
+            return
 
+        # Update player
+        self.player.updatePlayer()
 
+        # Checking player invincible time
+        if self.player_invi_dur != 0:
+            self.player_invi_dur -= 1
+        elif self.hyperspace == 0:
+            self.player_state = 0
 
-            
+        # Reset display
+        self.gameDisplayFill(self.black)
+
+        # Hyperspace
+        if self.hyperspace != 0:
+            self.player_state = 1
+            self.hyperspace -= 1
+            if self.hyperspace == 1:
+                self.player.x = random.randrange(0, self.display_width)
+                self.player.y = random.randrange(0, self.display_height)
+
+        # Check for collision w/ asteroid
+        for a in self.asteroids:
+            a.updateAsteroid()
+            if self.player_state != 1:
+                if self.isColliding(self.player.x, self.player.y, a.x, a.y, a.size):
+                    # Create ship fragments
+                    self.player_pieces.append(self.deadPlayer(self.player.x, self.player.y, 5 * self.player_size / (2 * math.cos(math.atan(1 / 3)))))
+                    self.player_pieces.append(self.deadPlayer(self.player.x, self.player.y, 5 * self.player_size / (2 * math.cos(math.atan(1 / 3)))))
+                    self.player_pieces.append(self.deadPlayer(self.player.x, self.player.y, self.player_size))
+
+                    # Kill player
+                    self.player_state = 1
+                    self.player_dying_delay = 30
+                    self.player_invi_dur = 120
+                    self.player.killPlayer()
+
+                    if self.live != 0:
+                        self.live -= 1
+                    else:
+                        self.gameState = 1
+
+                    # Split asteroid
+                    if a.t == "Large":
+                        self.asteroids.append(Asteroid(a.x, a.y, "Normal"))
+                        self.asteroids.append(Asteroid(a.x, a.y, "Normal"))
+                        self.score += 20
+                    elif a.t == "Normal":
+                        self.asteroids.append(Asteroid(a.x, a.y, "Small"))
+                        self.asteroids.append(Asteroid(a.x, a.y, "Small"))
+                        self.score += 50
+
+                    else:
+                        self.score += 100
+
+                    self.asteroids.remove(a)
+
+        # Update ship fragments
+        for f in self.player_pieces:
+            f.updateDeadPlayer()
+            if f.x > self.display_width or f.x < 0 or f.y > self.display_height or f.y < 0:
+                self.player_pieces.remove(f)
+
+        # Check for end of stage
+        if len(self.asteroids) == 0 and self.saucer.state == "Dead":
+            if self.next_level_delay < 30:
+                self.next_level_delay += 1
+            else:
+                self.stage += 1
+                self.intensity = 0
+                # Spawn asteroid away of center
+                for i in range(self.stage):
+                    xTo = self.display_width / 2
+                    yTo = self.display_height / 2
+                    while xTo - self.display_width / 2 < self.display_width / 4 and yTo - self.display_height / 2 < self.display_height / 4:
+                        xTo = random.randrange(0, self.display_width)
+                        yTo = random.randrange(0, self.display_height)
+                    self.asteroids.append(Asteroid(xTo, yTo, "Large"))
+                next_level_delay = 0
+
+        # Update intensity
+        if intensity < self.stage * 450:
+            intensity += 1
+
+        # Saucer
+        if self.saucer.state == "Dead":
+            if random.randint(0, 6000) <= (self.intensity * 2) / (self.stage * 9) and self.next_level_delay == 0:
+                self.saucer.createSaucer()
+                # Only small saucers >40000
+                if self.score >= 40000:
+                    self.saucer.type = "Small"
+        else:
+            # Set saucer targer dir
+            acc = self.small_saucer_accuracy * 4 / self.stage
+            self.saucer.bdir = math.degrees(math.atan2(-self.saucer.y + self.player.y, -self.saucer.x + self.player.x) + math.radians(random.uniform(acc, -acc)))
+
+            self.saucer.updateSaucer()
+            self.saucer.drawSaucer()
+
+            # Check for collision w/ asteroid
+            for a in self.asteroids:
+                if self.isColliding(self.saucer.x, self.saucer.y, a.x, a.y, a.size + self.saucer.size):
+                    # Set saucer state
+                    self.saucer.state = "Dead"
+
+                    # Split asteroid
+                    if a.t == "Large":
+                        self.asteroids.append(Asteroid(a.x, a.y, "Normal"))
+                        self.asteroids.append(Asteroid(a.x, a.y, "Normal"))
+
+                    elif a.t == "Normal":
+                        self.asteroids.append(Asteroid(a.x, a.y, "Small"))
+                        self.asteroids.append(Asteroid(a.x, a.y, "Small"))
+
+                    self.asteroids.remove(a)
+
+            # Check for collision w/ bullet
+            for b in self.bullets:
+                if self.isColliding(b.x, b.y, self.saucer.x, self.saucer.y, self.saucer.size):
+                    # Add points
+                    if self.saucer.type == "Large":
+                        self.score += 200
+                    else:
+                        self.score += 1000
+
+                    # Set saucer state
+                    self.saucer.state = "Dead"
+
+                    # Remove bullet
+                    self.bullets.remove(b)
+
+            # Check collision w/ player
+            if self.isColliding(self.saucer.x, self.saucer.y, self.player.x, self.player.y, self.saucer.size):
+                if self.player_state != 1:
+                    # Create ship fragments
+                    self.player_pieces.append(self.deadPlayer(self.player.x, self.player.y, 5 * self.player_size / (2 * math.cos(math.atan(1 / 3)))))
+                    self.player_pieces.append(self.deadPlayer(self.player.x, self.player.y, 5 * self.player_size / (2 * math.cos(math.atan(1 / 3)))))
+                    self.player_pieces.append(self.deadPlayer(self.player.x, self.player.y, self.player_size))
+
+                    # Kill player
+                    self.player_state = 1
+                    self.player_dying_delay = 30
+                    self.player_invi_dur = 120
+                    self.player.killPlayer()
+
+                    if self.live != 0:
+                        self.live -= 1
+                    else:
+                        self.gameState = 1
+
+            # Saucer's bullets
+            for b in self.saucer.bullets:
+                # Update bullets
+                b.updateBullet()
+
+                # Check for collision w/ asteroids
+                for a in self.asteroids:
+                    if self.isColliding(b.x, b.y, a.x, a.y, a.size):
+                        # Split asteroid
+                        if a.t == "Large":
+                            self.asteroids.append(Asteroid(a.x, a.y, "Normal"))
+                            self.asteroids.append(Asteroid(a.x, a.y, "Normal"))
+
+                        elif a.t == "Normal":
+                            self.asteroids.append(Asteroid(a.x, a.y, "Small"))
+                            self.asteroids.append(Asteroid(a.x, a.y, "Small"))
+
+                        # Remove asteroid and bullet
+                        self.asteroids.remove(a)
+                        self.saucer.bullets.remove(b)
+
+                        break
+
+                # Check for collision w/ player
+                if self.isColliding(self.player.x, self.player.y, b.x, b.y, 5):
+                    if self.player_state != 1:
+                        # Create ship fragments
+                        self.player_pieces.append(self.deadPlayer(self.player.x, self.player.y, 5 * self.player_size / (2 * math.cos(math.atan(1 / 3)))))
+                        self.player_pieces.append(self.deadPlayer(self.player.x, self.player.y, 5 * self.player_size / (2 * math.cos(math.atan(1 / 3)))))
+                        self.player_pieces.append(self.deadPlayer(self.player.x, self.player.y, self.player_size))
+
+                        # Kill player
+                        self.player_state = 1
+                        self.player_dying_delay = 30
+                        self.player_invi_dur = 120
+                        self.player.killPlayer()
+
+                        if self.live != 0:
+                            self.live -= 1
+                        else:
+                            self.gameState = 1
+
+                        # Remove bullet
+                        if b in self.saucer.bullets:
+                            self.saucer.bullets.remove(b)
+
+                if b.life <= 0:
+                    try:
+                        self.saucer.bullets.remove(b)
+                    except ValueError:
+                        continue
+
+        # Bullets
+        for b in self.bullets:
+            # Update bullets
+            b.updateBullet()
+
+            # Check for bullets collide w/ asteroid
+            for a in self.asteroids:
+                if b.x > a.x - a.size and b.x < a.x + a.size and b.y > a.y - a.size and b.y < a.y + a.size:
+                    # Split asteroid
+                    if a.t == "Large":
+                        self.asteroids.append(Asteroid(a.x, a.y, "Normal"))
+                        self.asteroids.append(Asteroid(a.x, a.y, "Normal"))
+                        self.score += 20
+
+                    elif a.t == "Normal":
+                        self.asteroids.append(Asteroid(a.x, a.y, "Small"))
+                        self.asteroids.append(Asteroid(a.x, a.y, "Small"))
+                        self.score += 50
+
+                    else:
+                        self.score += 100
+
+                    self.asteroids.remove(a)
+                    self.bullets.remove(b)
+
+                    break
+
+            # Destroying bullets
+            if b.life <= 0:
+                try:
+                    self.bullets.remove(b)
+                except ValueError:
+                    continue
+
+        # Extra live
+        if self.score > self.oneUp_multiplier * 10000:
+            self.oneUp_multiplier += 1
+            #live += 1
+            self.playOneUpSFX = 60
+        # Play sfx
+        if self.playOneUpSFX > 0:
+            self.playOneUpSFX -= 1
+
+        # Draw player
+        if self.gameState != 1:
+            if self.player_state == 1:
+                if self.hyperspace == 0:
+                    if self.player_dying_delay == 0:
+                        if self.player_blink < 5:
+                            if self.player_blink == 0:
+                                self.player_blink = 10
+                            else:
+                                self.player.drawPlayer()
+                        self.player_blink -= 1
+                    else:
+                        self.player_dying_delay -= 1
+            else:
+                self.player.drawPlayer()
+        else:
+
+            self.live = -1
+
+        # Draw score
+        self.drawText(str(self.score), self.white, 60, 20, 40, False)
+
+        # Draw Lives
+        for l in range(self.live + 1):
+            Player(75 + l * 25, 75).drawPlayer()
+
+        # Update screen
+        self.pygameDisplayUpdate()
+
+        # Tick fps
+        self.timer.tick(self.FPS)
+        print(self.timer.get_fps(), self.gameState)
+
+                
